113233b451af4bfef42a54f5257841e5a6476983
Flavius Dumitrascu <flavius_dumi@yahoo.com>
2016-03-19 16:16:47 +0200

    Add first version of server

---
+++ Server/PlaneCrush/PlaneCrush/HandleClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlaneCrush
+{
+    class HandleClient
+    {
+        private Hashtable clientsList;
+        private TcpClient clientSocket;
+        private string clientName;
+
+        public HandleClient(TcpClient clientSocket, string clientName, Hashtable clientsList)
+        {
+            this.clientSocket = clientSocket;
+            this.clientName = clientName;
+            this.clientsList = clientsList;
+
+            Thread ctThread = new Thread(listenToClient);
+            ctThread.Start();
+        }
+
+        private void listenToClient()
+        {
+            while (true) {
+                NetworkStream networkStream = clientSocket.GetStream();
+                Byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+                String dataFromClient;
+
+                networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+                dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+
+                msg("Client - " + clientName + ": " + dataFromClient);
+            }
+        }
+
+        private void msg(String ms)
+        {
+            ms.Trim();
+            Console.WriteLine(">> " + ms);
+        }
+
+        private void broadcastMsg(string uName, string msg, bool flag)
+        {
+            foreach (DictionaryEntry client in clientsList)
+            {
+                TcpClient broadcastSocket = (TcpClient)client.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = flag ? Encoding.ASCII.GetBytes(uName + ": " + msg) : Encoding.ASCII.GetBytes(msg);
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+    }
+}
--- Server/PlaneCrush/PlaneCrush/Program.cs
+++ Server/PlaneCrush/PlaneCrush/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PlaneCrush
+﻿namespace PlaneCrush
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Server s = new Server();
+            s.startServer();
         }
     }
 }
