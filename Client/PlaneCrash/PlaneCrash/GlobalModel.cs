4422e616f9df403e941da35307765d6350873f05
George Chiricuta <george.chiricuta@napa.fi>
2016-03-25 15:05:15 +0200

    Create planes on map

---
+++ Client/PlaneCrash/PlaneCrash/GlobalModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace PlaneCrash {
+    [ImplementPropertyChanged]
+    public class GlobalModel {
+        //public Direction PlaneDirection { get; set; }
+
+        public GlobalModel() { 
+        }
+    }
+
+    //public enum Direction {
+    //    Up,Left,Right,Down
+    //}
+}
--- Client/PlaneCrash/PlaneCrash/MainWindow.xaml.cs
+++ Client/PlaneCrash/PlaneCrash/MainWindow.xaml.cs
@@ -20,15 +20,336 @@ namespace PlaneCrash
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Direction PlaneDirection { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
-            WindowState = WindowState.Maximized;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateMap(SelfPlaneMap, Brushes.Violet, true);
+            CreateMap(HitTargetMap, Brushes.SkyBlue, false);
         }
 
         private void RotateLeft_Click(object sender, RoutedEventArgs e)
         {
-
+            PlaneImage.RenderTransform = new RotateTransform(-90);
+            PlaneDirection = Direction.Left;
         }
+
+        private void RotateDownClick(object sender, RoutedEventArgs e)
+        {
+            PlaneImage.RenderTransform = new RotateTransform(180);
+            PlaneDirection = Direction.Down;
+        }
+
+        private void RotateRightClick(object sender, RoutedEventArgs e)
+        {
+            PlaneImage.RenderTransform = new RotateTransform(90);
+            PlaneDirection = Direction.Right;
+        }
+
+        private void RotateUpClick(object sender, RoutedEventArgs e)
+        {
+            PlaneImage.RenderTransform = new RotateTransform(0);
+            PlaneDirection = Direction.Up;
+        }
+
+        private void CreateMap(Grid gridMap, Brush mapColor, bool allowDrop)
+        {
+            for (int rowIndex = 0; rowIndex < 10; rowIndex++)
+            {
+                RowDefinition row = new RowDefinition();
+                gridMap.RowDefinitions.Add(row);
+            }
+
+            for (int columnIndex = 0; columnIndex < 10; columnIndex++)
+            {
+                ColumnDefinition colum = new ColumnDefinition();
+                gridMap.ColumnDefinitions.Add(colum);
+            }
+
+            int k = 0;
+            for (int rowIndex = 0; rowIndex < 10; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 10; columnIndex++)
+                {
+                    k++;
+                    CustomButton placeHolder = new CustomButton() { Content = k.ToString() };
+                    placeHolder.Uid = k.ToString();
+                    placeHolder.Drop += PlaceHolder_Drop;
+                    placeHolder.DragEnter += PlaceHolder_DragEnter;
+                    placeHolder.AllowDrop = allowDrop;
+                    placeHolder.Background = mapColor;
+
+                    placeHolder.SetValue(Grid.RowProperty, rowIndex);
+                    placeHolder.SetValue(Grid.ColumnProperty, columnIndex);
+
+                    gridMap.Children.Add(placeHolder);
+                }
+            }
+        }
+
+        private void PlaceHolder_DragEnter(object sender, DragEventArgs e)
+        {
+            ClearSelection();
+            var btn = sender as CustomButton;
+            int headId = Convert.ToInt32(btn.Content);
+            var data = e.Data.GetData("Direction");
+
+            DrawPlane((Direction)data, headId, true, Brushes.Red);
+        }
+
+        private void PlaceHolder_Drop(object sender, DragEventArgs e)
+        {
+            var btn = sender as CustomButton;
+            int headId = Convert.ToInt32(btn.Content);
+            var data = e.Data.GetData("Direction");
+
+            DrawPlane((Direction)data, headId, false, Brushes.Yellow);
+        }
+
+        private void ClearSelection()
+        {
+            var btns = SelfPlaneMap.Children.Cast<UIElement>().ToList();
+            foreach (var item in btns)
+            {
+                var btn = item as CustomButton;
+                if (btn.Background == Brushes.Yellow && btn.IsSelected == true)
+                {
+                    btn.Background = Brushes.Yellow;
+                }
+                else if (btn.Background == Brushes.Red && btn.IsSelected == false)
+                {
+                    btn.Background = Brushes.Violet;
+                }
+            }
+        }
+
+        private void DrawPlane(Direction planeDirection, int headId, bool preview, Brush planeColor)
+        {
+            try
+            {
+                switch (planeDirection)
+                {
+                    case Direction.Up:
+                        PlaneCoordinates headUpCoord = GetHeadUpPlaneCoord(headId);
+                        if (!InvalidCoordonateFound(headUpCoord))
+                        {
+                            CreatePlane(headUpCoord, planeColor, preview);
+                        }
+                        break;
+                    case Direction.Down:
+                        PlaneCoordinates headDownCoord = GetHeadDownPlaneCoord(headId);
+                        if (!InvalidCoordonateFound(headDownCoord))
+                        {
+                            CreatePlane(headDownCoord, planeColor, preview);
+                        }
+                        break;
+                    case Direction.Left:
+                        PlaneCoordinates headLeftCoord = GetHeadLeftPlaneCoord(headId);
+                        if (!InvalidCoordonateFound(headLeftCoord))
+                        {
+                            CreatePlane(headLeftCoord, planeColor, preview);
+                        }
+                        break;
+                    case Direction.Right:
+                        PlaneCoordinates headRightCoord = GetHeadRightPlaneCoord(headId);
+                        if (!InvalidCoordonateFound(headRightCoord))
+                        {
+                            CreatePlane(headRightCoord, planeColor, preview);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private bool InvalidCoordonateFound(PlaneCoordinates planeCoordinates)
+        {
+            CustomButton headBtn = GetButtonById(planeCoordinates.HeadId);
+            CustomButton cellCenterFrontBtn = GetButtonById(planeCoordinates.CellCenterFrontId);
+            CustomButton cellLeftFrontWingBtn = GetButtonById(planeCoordinates.CellLeftFrontWingId);
+            CustomButton cellRightFrontWingBtn = GetButtonById(planeCoordinates.CellRightFrontWingId);
+            CustomButton cellCenterMiddleBtn = GetButtonById(planeCoordinates.CellCenterMiddleId);
+            CustomButton cellCenterBackBtn = GetButtonById(planeCoordinates.CellCenterBackId);
+            CustomButton cellLeftBackWingBtn = GetButtonById(planeCoordinates.CellLeftBackWingId);
+            CustomButton cellRightBackWingBtn = GetButtonById(planeCoordinates.CellRightBackWingId);
+
+            if (headBtn.IsSelected == true ||
+                cellCenterFrontBtn.IsSelected == true ||
+                cellLeftFrontWingBtn.IsSelected == true ||
+                cellRightFrontWingBtn.IsSelected == true ||
+                cellCenterMiddleBtn.IsSelected == true ||
+                cellCenterBackBtn.IsSelected == true ||
+                cellLeftBackWingBtn.IsSelected == true ||
+                cellRightBackWingBtn.IsSelected == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private PlaneCoordinates GetHeadRightPlaneCoord(int headId)
+        {
+            PlaneCoordinates pCoord = new PlaneCoordinates();
+            pCoord.HeadId = headId;
+            pCoord.CellCenterFrontId = pCoord.HeadId - 1;
+            pCoord.CellLeftFrontWingId = pCoord.CellCenterFrontId - 10;
+            pCoord.CellRightFrontWingId = pCoord.CellCenterFrontId + 10;
+            pCoord.CellCenterMiddleId = pCoord.CellCenterFrontId - 1;
+            pCoord.CellCenterBackId = pCoord.CellCenterMiddleId - 1;
+            pCoord.CellLeftBackWingId = pCoord.CellCenterBackId - 10;
+            pCoord.CellRightBackWingId = pCoord.CellCenterBackId + 10;
+            return pCoord;
+        }
+
+        private PlaneCoordinates GetHeadDownPlaneCoord(int headId)
+        {
+            PlaneCoordinates pCoord = new PlaneCoordinates();
+            pCoord.HeadId = headId;
+            pCoord.CellCenterFrontId = pCoord.HeadId - 10;
+            pCoord.CellLeftFrontWingId = pCoord.CellCenterFrontId + 1;
+            pCoord.CellRightFrontWingId = pCoord.CellCenterFrontId - 1;
+            pCoord.CellCenterMiddleId = pCoord.CellCenterFrontId - 10;
+            pCoord.CellCenterBackId = pCoord.CellCenterMiddleId - 10;
+            pCoord.CellLeftBackWingId = pCoord.CellCenterBackId + 1;
+            pCoord.CellRightBackWingId = pCoord.CellCenterBackId - 1;
+            return pCoord;
+        }
+
+        private PlaneCoordinates GetHeadLeftPlaneCoord(int headId)
+        {
+            PlaneCoordinates pCoord = new PlaneCoordinates();
+            pCoord.HeadId = headId;
+            pCoord.CellCenterFrontId = pCoord.HeadId + 1;
+            pCoord.CellLeftFrontWingId = pCoord.CellCenterFrontId + 10;
+            pCoord.CellRightFrontWingId = pCoord.CellCenterFrontId - 10;
+            pCoord.CellCenterMiddleId = pCoord.CellCenterFrontId + 1;
+            pCoord.CellCenterBackId = pCoord.CellCenterMiddleId + 1;
+            pCoord.CellLeftBackWingId = pCoord.CellCenterBackId + 10;
+            pCoord.CellRightBackWingId = pCoord.CellCenterBackId - 10;
+            return pCoord;
+        }
+
+        private PlaneCoordinates GetHeadUpPlaneCoord(int headId)
+        {
+            PlaneCoordinates pCoord = new PlaneCoordinates();
+            pCoord.HeadId = headId;
+            pCoord.CellCenterFrontId = pCoord.HeadId + 10;
+            pCoord.CellLeftFrontWingId = pCoord.CellCenterFrontId - 1;
+            pCoord.CellRightFrontWingId = pCoord.CellCenterFrontId + 1;
+            pCoord.CellCenterMiddleId = pCoord.CellCenterFrontId + 10;
+            pCoord.CellCenterBackId = pCoord.CellCenterMiddleId + 10;
+            pCoord.CellLeftBackWingId = pCoord.CellCenterBackId - 1;
+            pCoord.CellRightBackWingId = pCoord.CellCenterBackId + 1;
+            return pCoord;
+        }
+
+        private void CreatePlane(PlaneCoordinates planeCoordinates, Brush planeColor, bool preview)
+        {
+            CustomButton headBtn = GetButtonById(planeCoordinates.HeadId);
+            if (headBtn != null)
+            {
+                headBtn.Background = planeColor;
+
+                CustomButton cellCenterFrontBtn = GetButtonById(planeCoordinates.CellCenterFrontId);
+                cellCenterFrontBtn.Background = planeColor;
+
+                CustomButton cellLeftFrontWingBtn = GetButtonById(planeCoordinates.CellLeftFrontWingId);
+                cellLeftFrontWingBtn.Background = planeColor;
+
+                CustomButton cellRightFrontWingBtn = GetButtonById(planeCoordinates.CellRightFrontWingId);
+                cellRightFrontWingBtn.Background = planeColor;
+
+                CustomButton cellCenterMiddleBtn = GetButtonById(planeCoordinates.CellCenterMiddleId);
+                cellCenterMiddleBtn.Background = planeColor;
+
+                CustomButton cellCenterBackBtn = GetButtonById(planeCoordinates.CellCenterBackId);
+                cellCenterBackBtn.Background = planeColor;
+
+                CustomButton cellLeftBackWingBtn = GetButtonById(planeCoordinates.CellLeftBackWingId);
+                cellLeftBackWingBtn.Background = planeColor;
+
+                CustomButton cellRightBackWingBtn = GetButtonById(planeCoordinates.CellRightBackWingId);
+                cellRightBackWingBtn.Background = planeColor;
+
+                if (!preview)
+                {
+                    headBtn.IsSelected =
+                    cellCenterFrontBtn.IsSelected =
+                    cellLeftFrontWingBtn.IsSelected =
+                    cellRightFrontWingBtn.IsSelected =
+                    cellCenterMiddleBtn.IsSelected =
+                    cellCenterBackBtn.IsSelected =
+                    cellLeftBackWingBtn.IsSelected =
+                    cellRightBackWingBtn.IsSelected = true;
+                }
+                else
+                {
+                    headBtn.IsSelected =
+                    cellCenterFrontBtn.IsSelected =
+                    cellLeftFrontWingBtn.IsSelected =
+                    cellRightFrontWingBtn.IsSelected =
+                    cellCenterMiddleBtn.IsSelected =
+                    cellCenterBackBtn.IsSelected =
+                    cellLeftBackWingBtn.IsSelected =
+                    cellRightBackWingBtn.IsSelected = false;
+                }
+            }
+        }
+
+        private CustomButton GetButtonById(int id)
+        {
+            var btns = SelfPlaneMap.Children.Cast<UIElement>().ToList();
+            var foundBtn = btns.Where(b => b.Uid == id.ToString()).FirstOrDefault();
+            if (foundBtn != null)
+            {
+                return foundBtn as CustomButton;
+            }
+            return null;
+        }
+
+        private void Plane_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            DataObject dragObjectData = new DataObject("Direction", PlaneDirection);
+            DragDrop.DoDragDrop(image, dragObjectData, DragDropEffects.Move);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var uiCollection = SelfPlaneMap.Children.Cast<UIElement>().ToList();
+            foreach (var item in uiCollection)
+            {
+                CustomButton btn = item as CustomButton;
+                btn.Background = Brushes.Violet;
+                btn.IsSelected = false;
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        Up, Left, Right, Down
+    }
+
+    public class PlaneCoordinates
+    {
+        public int HeadId { get; set; }
+        public int CellCenterFrontId { get; set; }
+        public int CellLeftFrontWingId { get; set; }
+        public int CellRightFrontWingId { get; set; }
+        public int CellCenterMiddleId { get; set; }
+        public int CellCenterBackId { get; set; }
+        public int CellLeftBackWingId { get; set; }
+        public int CellRightBackWingId { get; set; }
     }
 }
