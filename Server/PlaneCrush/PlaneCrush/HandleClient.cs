6aa6511fde89bf0bd96295d9ea6d7907af560d41
Flavius Dumitrascu <flavius_dumi@yahoo.com>
2016-05-21 10:12:37 +0300

    Server phases

--- Server/PlaneCrush/PlaneCrush/HandleClient.cs
+++ Server/PlaneCrush/PlaneCrush/HandleClient.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using PlaneCrash;
+using System;
+using System.IO;
 using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using Wrapper;
 
 namespace PlaneCrush
 {
@@ -29,17 +29,52 @@ namespace PlaneCrush
             while (true) {
                 NetworkStream networkStream = clientSocket.GetStream();
                 Byte[] bytesFrom = new byte[clientSocket.Available];
+                networkStream.Read(bytesFrom, 0, clientSocket.Available);
 
-                try
-                {
-                    networkStream.Read(bytesFrom, 0, clientSocket.Available);
+                if (bytesFrom.Length > 0) {
+                    if (Server.readyPlayers < 2)
+                    {
+                        MessageWrapper message = ByteArrayToObject(bytesFrom);
 
-                    msg("Client - " + clientName + ": object");
-                    Server.broadcastMsg(clientName, bytesFrom);
+                        msg("Client - " + clientName + ": " + message.PlanesReady);
+                        if (message.PlanesReady)
+                        {
+                            Server.readyPlayers++;
+                            msg("Client - " + clientName + ": READY");
+                        }
+                    }
+                    else if (Server.phase != Phases.ACKNOWLEDGE)
+                    {
+                        try
+                        {
+                            msg("Client - " + clientName + ": object");
+                            Server.broadcastMsg(clientName, bytesFrom);
+                        }
+                        catch (Exception) { }
+                        finally
+                        {
+                            switch (Server.phase) {
+                                case Phases.ATACK:
+                                    Server.phase = Phases.HIT;
+                                    break;
+                                case Phases.HIT:
+                                    Server.phase = Phases.LOSE;
+                                    break;
+                                case Phases.LOSE:
+                                    Server.phase = Phases.ACKNOWLEDGE;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (Server.readyPlayers == 2 && Server.phase == Phases.ACKNOWLEDGE)
+                    {
+                        Server.sendActivePlayerMsg();
+                        Server.phase = Phases.ATACK;
+                    }
                 }
-                catch (Exception) {
-                }
-                
             }
         }
 
@@ -47,6 +82,29 @@ namespace PlaneCrush
         {
             ms.Trim();
             Console.WriteLine(">> " + ms);
+        }
+
+        private MessageWrapper ByteArrayToObject(byte[] arrBytes)
+        {
+            MessageWrapper ReturnValue;
+            using (var _MemoryStream = new MemoryStream(arrBytes))
+            {
+                IFormatter _BinaryFormatter = new BinaryFormatter();
+                ReturnValue = (MessageWrapper)_BinaryFormatter.Deserialize(_MemoryStream);
+            }
+            return ReturnValue;
+        }
+
+        public byte[] ObjectToByteArray(Message obj)
+        {
+            byte[] bytes;
+            using (var _MemoryStream = new MemoryStream())
+            {
+                IFormatter _BinaryFormatter = new BinaryFormatter();
+                _BinaryFormatter.Serialize(_MemoryStream, obj);
+                bytes = _MemoryStream.ToArray();
+            }
+            return bytes;
         }
     }
 }
---
+++ Server/PlaneCrush/PlaneCrush/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneCrash
+{
+    [Serializable]
+    public class Message
+    {
+        public int CellToHit { get; set; }
+        public bool Atacker { get; set; }
+
+        public bool PlanesReady { get; set; }
+    }
+}
