a05eb05e61eb7ec71814bb4d0a7a5ce2fcedac93
Flavius Dumitrascu <flavius_dumi@yahoo.com>
2016-05-21 16:05:17 +0300

    Add phases to Wraper

--- Client/PlaneCrash/Wrapper/MessageWrapper.cs
+++ Client/PlaneCrash/Wrapper/MessageWrapper.cs
@@ -9,10 +9,18 @@ namespace Wrapper
     [Serializable]
     public class MessageWrapper
     {
+        public enum Phases
+        {
+            ACKNOWLEDGE,
+            ATACK,
+            HIT,
+            LOSE
+        }
         public int CellToHit { get; set; }
         public bool Atacker { get; set; }
 
         public bool PlanesReady { get; set; }
         public string ActivePlayer { get; set; }
+        public Phases Phase { get; set; }
     }
 }
